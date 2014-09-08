bab7312aa10d13fdf0d766062b5725b53561db24
hjgode <hjgode@gmail.com>
2014-09-08 16:21:07 +0200

    extended MP3SERIAL class

--- MP3SERIAL.cpp
+++ MP3SERIAL.cpp
@@ -1,27 +1,32 @@
+#include <Arduino.h>
 #include "MP3SERIAL.h"
-#include <SoftwareSerial.h>
+#include "SoftwareSerial.h"
 
 /*
 class to control grove _mp3serial serial player
 */
 
-SoftwareSerial _mp3serial;
-
 int _iTX=2;
 int _iRX=3;
+
+SoftwareSerial _mp3serial=SoftwareSerial(_iRX,_iTX);
 
 const uint8_t END=0xFF;
 uint8_t aPauseToggle[]={0x7e,0x02,0xa3,0x7e, END};
 
 //private func to check for OK code 
 boolean isOKcode(){
-	while(_mp3serial.available())
+	int countDown=10;
+	while(_mp3serial.available() && countDown>0)
 	{
 	    if (0xA0==_mp3serial.read())
 	        return true;
 	    else 
 	        return false;
+	    delay(10);
+	    countDown--;
     }
+	return false;
 }
 
 //<<constructor>> setup the comm PINs
@@ -38,42 +43,105 @@ MP3SERIAL::~MP3SERIAL(){
 }
 
 //play
-void MP3SERIAL::play(){
+bool MP3SERIAL::play(){
+	return next();
 }
 
 //stop
-void MP3SERIAL::stop(){
+bool MP3SERIAL::stop(){
+	//next
+	_mp3serial.write(0x7E);
+	_mp3serial.write(0x02);
+	_mp3serial.write(0xA4);
+	_mp3serial.write(0x7E);
+	 delay(10);
+	 return isOKcode();
 }
 
 //next
-void MP3SERIAL::next(){
+bool MP3SERIAL::next(){
+	_mp3serial.write(0x7E);
+	_mp3serial.write(0x02);
+	_mp3serial.write(0xA5);
+	_mp3serial.write(0x7E);
+	 delay(10);
+	 return isOKcode();
+}
+
+//next
+bool MP3SERIAL::prev(){
+	_mp3serial.write(0x7E);
+	_mp3serial.write(0x02);
+	_mp3serial.write(0xA6);
+	_mp3serial.write(0x7E);
+	 delay(10);
+	 return isOKcode();
+}
+
+bool MP3SERIAL::pauseToggle(){
+	_mp3serial.write(0x7E);
+	_mp3serial.write(0x02);
+	_mp3serial.write(0xA3);
+	_mp3serial.write(0x7E);
+	 delay(10);
+	 return isOKcode();
 }
 
 //setVolume
-boolean MP3SERIAL::setVolume(int iVolume){
+//There are 32 volume levels from 00 to 31. 00 is mute, and 31 for the maximum volume.
+bool MP3SERIAL::setVolume(byte iVolume){
+	if(iVolume>31)
+		iVolume=31;
     _mp3serial.write(0x7E);
     _mp3serial.write(0x03);
     _mp3serial.write(0xA7);
     _mp3serial.write(iVolume);
     _mp3serial.write(0x7E);
     delay(10);
-    return ::isOKcode();
+    return isOKcode();
 }
 
+
+bool MP3SERIAL::setRepeatMode(repeatMode bMode){
+	if (((bMode==0x00)|(bMode==0x01)|(bMode==0x02)|(bMode==0x03))==false)
+	{
+		Serial.println("PlayMode Parameter Error! ");
+		return false;
+	}
+	_mp3serial.write(0x7E);
+	_mp3serial.write(0x03);
+	_mp3serial.write(0xA9);
+	_mp3serial.write((byte)bMode);
+	_mp3serial.write(0x7E);
+    delay(10);
+    return ::isOKcode();
+}
 //setRepeatAll
-void MP3SERIAL::setRepeatAll(){
+bool MP3SERIAL::setRepeatAll(){
+	return setRepeatMode(repeatAll);
 }
 
 //setRepeatSingle
-void MP3SERIAL::setRepeatSingle(){
+bool MP3SERIAL::setRepeatSingle(){
+	return setRepeatMode(repeatSingle);
 }
 
+//setRepeatSingle
+bool MP3SERIAL::setRepeatNone(){
+	return setRepeatMode(repeatNone);
+}
+
+bool MP3SERIAL::playByIndex(uint8_t lbyte){
+	playByIndex(0x00, lbyte);
+    delay(10);
+    return(isOKcode());
+}
 //play mp3 by index
 //Set the music index to play, the index is decided by the input sequence
 //of the music;
 //hbyte: the high byte of the index;
 //lbyte: the low byte of the index;
-boolean MP3SERIAL::playByIndex(uint8_t hbyte,uint8_t lbyte)
+bool MP3SERIAL::playByIndex(uint8_t hbyte,uint8_t lbyte)
 {
 	_mp3serial.write(0x7E);
 	_mp3serial.write(0x04);
@@ -94,6 +162,128 @@ boolean MP3SERIAL::playByIndex(uint8_t hbyte,uint8_t lbyte)
     return isOKcode();
 }
 
+byte MP3SERIAL::getVolume(){
+	byte bVolume=0;
+	_mp3serial.write(0x7E);
+	_mp3serial.write(0x02);
+	_mp3serial.write(0xC1);
+	_mp3serial.write(0x7E);
+    delay(10);
+    int maxCount=5;
+    //any data avilable?
+	while(_mp3serial.available() && maxCount>0)
+	{
+		//answer code?
+	    if (0xC1==_mp3serial.read())
+	    {
+	    	if(_mp3serial.available()){
+	    		bVolume=_mp3serial.read();
+	    		return bVolume;
+	    	}
+		    else
+		        return 0;
+	    }
+	    else
+	        return 0;
+	    maxCount--;
+    }
+	return 0;
+}
+// states:
+// 0x01=Play
+// 0x02=Stop
+// 0x03=Pause
+byte MP3SERIAL::getState(){
+	byte bState=0;
+	_mp3serial.write(0x7E);
+	_mp3serial.write(0x02);
+	_mp3serial.write(0xC2);
+	_mp3serial.write(0x7E);
+    delay(10);
+    int maxCount=5;
+    //any data avilable?
+	while(_mp3serial.available() && maxCount>0)
+	{
+		//answer code?
+	    if (0xC2==_mp3serial.read())
+	    {
+	    	if(_mp3serial.available()){
+	    		bState=_mp3serial.read();
+	    		return bState;
+	    	}
+		    else
+		        return 0;
+	    }
+	    else
+	        return 0;
+	    maxCount--;
+    }
+	return 0;
+}
+
+byte MP3SERIAL::getSDNumFiles(){
+	byte bAnswer=0;
+	_mp3serial.write(0x7E);
+	_mp3serial.write(0x02);
+	_mp3serial.write(0xC4);
+	_mp3serial.write(0x7E);
+    delay(10);
+    int maxCount=5;
+    //any data avilable?
+	while(_mp3serial.available() && maxCount>0)
+	{
+		//answer code?
+	    if (0xC4==_mp3serial.read())
+	    {
+	    	if(_mp3serial.available()){
+	    		bAnswer=_mp3serial.read();
+	    		return bAnswer;
+	    	}
+		    else
+		        return 0;
+	    }
+	    else
+	        return 0;
+	    maxCount--;
+    }
+	return 0;
+}
+
+int MP3SERIAL::getSDcurrentFile(){
+	byte bAnswerLow=0;
+	byte bAnswerHigh=0;
+	int iReturn=-1;
+	_mp3serial.write(0x7E);
+	_mp3serial.write(0x02);
+	_mp3serial.write(0xC6);
+	_mp3serial.write(0x7E);
+    delay(10);
+    int maxCount=5;
+    //any data avilable?
+	while(_mp3serial.available() && maxCount>0)
+	{
+		//answer code?
+	    if (0xC6==_mp3serial.read())
+	    {
+	    	if(_mp3serial.available()){
+	    		bAnswerHigh=_mp3serial.read();
+		    	if(_mp3serial.available()){
+		    		bAnswerLow=_mp3serial.read();
+		    		iReturn=(0xFF*bAnswerHigh)+bAnswerLow;
+		    		return iReturn;
+		    	}
+		    	else
+		    		return -1;
+	    	}
+		    else
+		        return -1;
+	    }
+	    else
+	        return -1;
+	    maxCount--;
+    }
+	return 0;
+}
 
 void MP3SERIAL::writeCommand(uint8_t* arrayBytes){
     
--- MP3SERIAL.h
+++ MP3SERIAL.h
@@ -3,6 +3,19 @@
      
 #include <Arduino.h> //It is very important to remember this! note that if you are using Arduino 1.0 IDE, change "WProgram.h" to "Arduino.h"
 
+enum repeatMode{
+	repeatNone=0,
+	repeatSingle=0x01,
+	repeatAll=0x02,
+	repeatRandom=0x03,
+};
+
+enum playState{
+	playStatePlay=0x01,
+	playStateStop=0x02,
+	playStatePause=0x03,
+};
+
 class MP3SERIAL {
     private:
 //        boolean isOKcode();
@@ -10,13 +23,22 @@ class MP3SERIAL {
     public:
         MP3SERIAL(int iRX, int iTX);
         ~MP3SERIAL();
-        void play();
-        void stop();
-        void next();
-        boolean setVolume(int iVol);
-        void setRepeatAll();
-        void setRepeatSingle();
-        boolean playByIndex(uint8_t hbyte,uint8_t lbyte);
+        bool play();
+        bool stop();
+        bool next();
+        bool prev();
+        bool pauseToggle();
+        bool setVolume(byte iVol);
+        byte getVolume();
+        byte getState();
+        byte getSDNumFiles();
+        int getSDcurrentFile();
+        bool setRepeatMode(repeatMode bMode);
+        bool setRepeatAll();
+        bool setRepeatSingle();
+        bool setRepeatNone();
+        bool playByIndex(uint8_t lbyte);
+        bool playByIndex(uint8_t hbyte,uint8_t lbyte);
 };
      
 #endif
